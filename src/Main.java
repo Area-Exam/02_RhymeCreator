46bd37416a9d6411252b07b6105435f448c37d22
Nisansa Dilushan de Silva <NisansaDdS@cse.mrt.ac.lk>
2017-06-23 20:24:19 -0700

    Main created

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        String source="../Output/words.txt";
+        String rejectedEmpty="../Output/rejectWordsEmpty.txt";
+        String rejectedNull="../Output/rejectWordsEmpty.txt";
+        String rhymes="../Output/rhymes.txt";
+        String machine="Rhymer";
+
+
+        if(args.length>0) {
+            machine = args[0];
+            source = args[1];
+            rejectedEmpty = args[2];
+            rejectedNull = args[3];
+            rhymes = args[4];
+        }
+
+        Crawler c=null;
+        if(machine.equalsIgnoreCase("Rhymer")) {
+            //c = new Rhymer(source, rejectedEmpty,rejectedNull,rhymes);
+        }
+
+        if(c!=null) {
+            c.run();
+        }
+        else{
+            System.out.println("Crawler not initialized");
+        }
+
     }
 }
